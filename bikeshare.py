dbc939f34f327461977006113a7733e361e4a2f8
E9970989 <AmarPandharpure@Eaton.com>
2020-10-09 14:04:03 +0530

    Updated comments in python file

--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,9 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 
 CITY=['chicago','new york city','washigton']
+
 MONTHS=['all','january','february','march','april','may','june']
+
 DAYS=['all','monday','tuesday','wednesday','thursday','friday','saturday','sunday']
 
 def get_filters():
@@ -21,7 +23,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input("Please enter city you like to analyze (" + ",".join(CITY)+" )?\n").lower()
         if city in CITY:
